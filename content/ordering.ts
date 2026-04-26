b9e02ba65258b7458c0b61b4c9eed42a1088f64e
Wht/Mld <125677434+Lrdmtns@users.noreply.github.com>
2026-04-26 21:25:09 +0000

    Update ordering.ts

--- content/ordering.ts
+++ content/ordering.ts
@@ -304,18 +304,14 @@ const MODULE_ORDERING: { [key in SectionID]: Chapter[] } = {
   usamo: [
     {
       name: 'Proof Writing',
-      items: ['proof-writing-basics'],
+      items: ['proof-writing-basics','strategy-writeup'],
     },
     {
-      name: 'Induction and Extremal Principle',
-      items: ['induction-extremal'],
+      name: 'Olympiad Combinatorics',
+      items: ['induction-extremal','graph-theory-intro'],
     },
     {
-      name: 'Graph Theory (Intro)',
-      items: ['graph-theory-intro'],
-    },
-    {
-      name: 'Advanced Inequalities',
+      name: 'Olympiad Algebra',
       items: ['inequalities-olympiad'],
     },
     {
@@ -325,10 +321,6 @@ const MODULE_ORDERING: { [key in SectionID]: Chapter[] } = {
     {
       name: 'Olympiad Geometry',
       items: ['olympiad-geometry'],
-    },
-    {
-      name: 'Strategy and Writeups',
-      items: ['strategy-writeup'],
     },
   ],
 };
